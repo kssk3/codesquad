b1795be1387ff0c65a55602832d97a55f249a15d
kssk3 <kss8014@daum.net>
2024-11-30 20:08:07 +0900

    chore LottoGame Class 생성

---
+++ src/LottoGame.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class LottoGame {
+    private static final int MAX_NUMBER = 45;
+    private final Random random;
+
+    public LottoGame() {
+        this.random = new Random();
+    }
+
+    
+}
