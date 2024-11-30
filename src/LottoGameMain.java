cce71c8fd8e015edc5192a260ff9e45a4b5b5714
kssk3 <kss8014@daum.net>
2024-11-30 17:48:56 +0900

    랜덤 번호 출력 v2

--- src/LottoGameMain.java
+++ src/LottoGameMain.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class LottoGameMain {
@@ -11,21 +12,32 @@ public class LottoGameMain {
 
     public static void main(String[] args) {
         List<Integer> numberList = new ArrayList<>();
+        List<Integer> autoNumberList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.println("1~45중 로또 번호를 6개 입력해주세요.");
 
         while (numberList.size() < LOTTO_SIZE) {
             int input = scanner.nextInt();
             validate(input);
-            if (numberList.contains(input)) {
-                System.out.println("이미 같은" + input + "이 있습니다. 다른 번호를 입력해주세요.");
-            } else {
+            if (!numberList.contains(input)) {
                 numberList.add(input);
+            } else {
+                System.out.println("이미 같은" + input + "이 있습니다. 다른 번호를 입력해주세요.");
             }
         }
         System.out.println(numberList);
-        scanner.close();
+
+        while (autoNumberList.size() < LOTTO_SIZE) {
+            int input = random.nextInt(MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER;
+            if (!autoNumberList.contains(input)) {
+                autoNumberList.add(input);
+            } else {
+                System.out.println("이미 같은" + input + "이 있습니다. 다른 번호를 입력해주세요.");
+            }
+        }
+        System.out.println(autoNumberList);
     }
 
     private static void validate(int input) {
--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -1,4 +1,11 @@
+import java.util.Random;
+
 public class RandomNumber {
 
+    private LottoNumber lottoNumber;
 
+    public static int randomNumber(int input) {
+        Random rand = new Random();
+        return rand.nextInt(input);
+    }
 }
