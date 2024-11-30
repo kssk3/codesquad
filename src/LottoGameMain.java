c9c81b6064299baf7a983a2895fd580bf83e2157
kssk3 <kss8014@daum.net>
2024-11-30 20:27:13 +0900

    refactor LottoGameMain class

--- src/LottoGameMain.java
+++ src/LottoGameMain.java
@@ -1,38 +1,32 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class LottoGameMain {
-    private static final String REQUEST_NUMBER = "1~45사이의 숫자만 입력 가능합니다.";
-    private static final int LOTTO_SIZE = 6;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
 
+    private static final int LOTTO_SIZE = 6;
 
     public static void main(String[] args) {
-        List<Integer> numberList = new ArrayList<>();
-        List<Integer> autoNumberList = new ArrayList<>();
-        List<Integer> winningNumberList = new ArrayList<>(numberList);
-
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        LottoGame game = new LottoGame();
 
         System.out.println("1~45중 로또 번호를 6개 입력해주세요.");
+        List<LottoNumber> playerNumbers = new ArrayList<>();
 
-        while (numberList.size() < LOTTO_SIZE) {
-            int input = scanner.nextInt();
-            if (validate(input)) {
-                if (!numberList.contains(input)) {
-                    numberList.add(input);
+        while (playerNumbers.size() < LOTTO_SIZE) {
+            try {
+                int input = scanner.nextInt();
+                LottoNumber number = new LottoNumber(input);
+                if (!playerNumbers.contains(number)) {
+                    playerNumbers.add(number);
                 } else {
                     System.out.println("이미 같은" + input + "이 있습니다. 다른 번호를 입력해주세요.");
                 }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
         }
-        Collections.sort(numberList);
-        System.out.println("플레이어의 숫자:" + numberList);
 
         while (autoNumberList.size() < LOTTO_SIZE) {
             int input = random.nextInt(MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER;
