ef0b53f9ebb8ab8c7362c3e1462518f6717d8e36
kssk3 <kss8014@daum.net>
2024-11-30 17:20:07 +0900

    1단계 플레이어 숫자 입력하기v1

--- src/LottoGameMain.java
+++ src/LottoGameMain.java
@@ -11,15 +11,23 @@ public class LottoGameMain {
 
     public static void main(String[] args) {
         List<Integer> numberList = new ArrayList<>();
-
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("1~45중 로또 번호를 6개 입력해주세요.");
 
-        while (true) {
+        while (numberList.size() < LOTTO_SIZE) {
             int input = scanner.nextInt();
-
+            validate(input);
+            if (numberList.contains(input)) {
+                System.out.println("이미 같은" + input + "이 있습니다. 다른 번호를 입력해주세요.");
+            } else {
+                numberList.add(input);
+            }
         }
+        System.out.println(numberList);
+        scanner.close();
     }
+
 
     private static void validate(int input) {
         if (input < MIN_NUMBER || input > MAX_NUMBER) {
