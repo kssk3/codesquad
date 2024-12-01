5366df6762392bbd7bd381cae58fe65f66631123
kssk3 <kss8014@daum.net>
2024-12-01 11:54:01 +0900

    Output Class 일치한 번호 개수 카운팅

--- src/LottoGameMain.java
+++ src/LottoGameMain.java
@@ -7,11 +7,7 @@ public class LottoGameMain {
     private static final int LOTTO_SIZE = 6;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        LottoGame game = new LottoGame();
-
-        System.out.println("1~45중 로또 번호를 6개 입력해주세요.");
-        List<LottoNumber> playerNumbers = new ArrayList<>();
-
-        }
+        Controller controller = new Controller();
+        controller.run();
+    }
 }
--- src/OutputView.java
+++ src/OutputView.java
@@ -6,4 +6,12 @@ public class OutputView {
     public void displayPlayerTicket(LottoTicket ticket) {
         System.out.println("플레이어번호: " + ticket);
     }
+
+    public void displayWinningTicket(LottoTicket ticket) {
+        System.out.println("당첨 로또 번호: " + ticket);
+    }
+
+    public void disPlayMatchCount(int count) {
+        System.out.println("일치한 번호의 개수: " + count);
+    }
 }
