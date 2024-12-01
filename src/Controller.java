9c52ed2c7fe02b81d42745f402a485731eccceb2
kssk3 <kss8014@daum.net>
2024-12-01 11:46:43 +0900

    [feat] Controller class 생성

---
+++ src/Controller.java
@@ -0,0 +1,17 @@
+public class Controller {
+    private LottoGame lottoGame;
+    private InputView inputView;
+    private OutputView outputView;
+
+    public Controller() {
+        this.lottoGame = new LottoGame();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        outputView.displayMessage("1~45중 로또 번호를 6개 입력하세요.");
+        LottoTicket playerTicket = inputView.getUserTicket();
+        outputView.displayPlayerTicket(playerTicket);
+    }
+}
