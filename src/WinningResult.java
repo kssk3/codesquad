1a23ef54b4b4c5397da280a26d88241e27c5b3e8
kssk3 <kss8014@daum.net>
2024-12-01 11:03:34 +0900

    WinningResult Class 추가

---
+++ src/WinningResult.java
@@ -0,0 +1,18 @@
+public class WinningResult {
+    private final int matchCount;
+    private final boolean bonusMatch;
+
+
+    public WinningResult(int matchCount, boolean bonusMatch) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean isBonusMatch() {
+        return bonusMatch;
+    }
+}
